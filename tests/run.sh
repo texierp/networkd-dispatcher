ea196873060bf709d07f4b397eb7f8fd6e941f7a
Clayton Craft <clayton@craftyguy.net>
2018-01-07 21:12:28 -0800

    tests: Add helper script

---
+++ tests/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function error() {
+	echo "ERROR: $1"
+	exit 1
+}
+
+# Required binaries/apps
+reqs=('pytest' 'coverage')
+for req in ${reqs[*]}; do
+	[[ -z "$(command -v $req)" ]] && error "$req not found"
+done
+# Check for pytest-cov module
+python -c "import pytest_cov" 2>/dev/null || error "pytest-cov module not found"
+
+pytest --cov=networkd_dispatcher -q tests
